33761230a9f115e78f7f2550d59a0cc9f9b7493f
RoibuRadu23 <roiburadu2003@gmail.com>
2025-11-24 14:05:59 +0200

    dataset++

--- data/raw/fragment1/surface_volume/script.sh
+++ data/raw/fragment1/surface_volume/script.sh
@@ -1,6 +1,6 @@
 BASE="https://dl.ash2txt.org/fragments/Frag1/PHercParis2Fr47.volpkg/working/54keV_exposed_surface/surface_volume"
 
-for i in $(seq -w 0 30); do
+for i in $(seq -w 1 30); do
     echo "Downloading $i.tif ..."
     wget "$BASE/$i.tif"
 done
